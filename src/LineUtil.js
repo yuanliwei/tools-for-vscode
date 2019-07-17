5a6950feb0ccdda522293251dda506b33a0297f0
yuanliwei <891789592@qq.com>
2019-07-17 16:45:25 +0800

    add line trimLeft trimRight & rename Order -> Sort

--- src/LineUtil.js
+++ src/LineUtil.js
@@ -8,13 +8,19 @@ module.exports = class LineUtil {
     static async lineRemoveEmpty(text) {
         return text.split('\n').filter(o => o.trim()).join('\n')
     }
-    static async lineOrderAsc(text) {
+    static async lineSortAsc(text) {
         return text.split('\n').sort().join('\n')
     }
-    static async lineOrderDesc(text) {
+    static async lineSortDesc(text) {
         return text.split('\n').sort().reverse().join('\n')
     }
     static async lineTrim(text) {
         return text.split('\n').map(o => o.trim()).join('\n')
+    }
+    static async lineTrimLeft(text) {
+        return text.split('\n').map(o => o.trimLeft()).join('\n')
+    }
+    static async lineTrimRight(text) {
+        return text.split('\n').map(o => o.trimRight()).join('\n')
     }
 }
