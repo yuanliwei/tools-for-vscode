acab8754b9b7d70378c4be83d2307d7bcb739793
yuanliwei <891789592@qq.com>
2024-02-07 15:09:50 +0800

    update test

--- test/runTest.js
+++ test/runTest.js
@@ -1,23 +1,24 @@
 import { resolve } from 'path'
 
 import { runTests } from '@vscode/test-electron'
+import { fileURLToPath } from 'url'
 
 async function main() {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = resolve(__dirname, '../');
+        const extensionDevelopmentPath = fileURLToPath(new URL('../', import.meta.url))
 
         // The path to the extension test script
         // Passed to --extensionTestsPath
-        const extensionTestsPath = resolve(__dirname, './suite/index');
+        const extensionTestsPath = fileURLToPath(new URL('./suite/index.js', import.meta.url))
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        await runTests({ extensionDevelopmentPath, extensionTestsPath })
     } catch (err) {
-        console.error('Failed to run tests');
-        process.exit(1);
+        console.error('Failed to run tests', err)
+        process.exit(1)
     }
 }
 
-main();
+main()
--- test/suite/index.js
+++ test/suite/index.js
@@ -1,42 +1,24 @@
-const path = require('path');
-const Mocha = require('mocha');
-const glob = require('glob');
+import { resolve } from 'path'
+import Mocha from 'mocha'
+import { glob } from 'glob'
+import { fileURLToPath } from 'url'
 
-function run() {
-	// Create the mocha test
-	const mocha = new Mocha({
-		ui: 'tdd',
-		color: true
-	});
+export async function run() {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+        color: true
+    })
 
-	const testsRoot = path.resolve(__dirname, '..');
+    const testsRoot = fileURLToPath(new URL('../', import.meta.url))
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
+    let files = await glob('**/**.test.js', { cwd: testsRoot })
 
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+    files.forEach(f => mocha.addFile(resolve(testsRoot, f)))
 
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				console.error(err);
-				e(err);
-			}
-		});
-	});
+    mocha.run(failures => {
+        if (failures > 0) {
+            throw new Error(`${failures} tests failed.`)
+        }
+    })
 }
-
-module.exports = {
-	run
-};
